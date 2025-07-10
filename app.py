ce8ff6edd9c3a68baf4346554449e90828ee68c8
leoAlflen <leonardo.alflen@hotmail.com>
2025-07-10 12:47:24 +0100

    feat: add delete api

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import _sqlite3 as sqlite3
 
 dbstring = 'stockDatabase.db'
 
+#create database
 def create_database():
     con = sqlite3.connect(dbstring)
     cur = con.cursor()
@@ -12,21 +13,29 @@ def create_database():
     con.commit()
     con.close()
 
+#insert records/rows/new items
 def insert_record(name, type, quantity, ml):
     con = sqlite3.connect(dbstring)
     cur = con.cursor()
-    serves = calculate_serves(type,quantity, ml)
+    serves = calculate_serves(type, ml)
     cur.execute('''INSERT INTO stock(Name, Type, Quantity, ML, Serves) VALUES(? ,? ,?, ?, ?)''', (name, type,quantity, ml, serves))
     con.commit()
     con.close()
 
-def calculate_serves(type,quantity, ml):
+#calculates the serves for each beverage
+def calculate_serves(type, ml):
      if type == 'Gin' or type == 'Whiskey' or type == 'Vodka':
-          serves = (ml / 35) * quantity
-     else: serves = (ml / 25) * quantity
+          serves = (ml / 35)
+     elif type == 'Keg':
+            serves = (ml / 500)
+     elif type == 'Syrup':
+        serves = (ml/25)
+     elif type == 'Bottle':
+          serves = ml / ml
 
      return serves    
 
+#retrieve records by type
 def retrieve_records(item_type):
     con = sqlite3.connect(dbstring)
     con.row_factory = sqlite3.Row
@@ -36,6 +45,7 @@ def retrieve_records(item_type):
     con.close()
     return records
 
+#retrieve records by name
 def retrieve_by_name(name):
     con = sqlite3.connect(dbstring)
     cur = con.cursor()
@@ -44,6 +54,7 @@ def retrieve_by_name(name):
     con.close()
     return records 
 
+#delete records/rows/items
 def delete_records(name):
 
         con = sqlite3.connect(dbstring)
@@ -51,8 +62,18 @@ def delete_records(name):
         cur.execute("DELETE FROM stock WHERE Name = ? ", (name,))
         con.commit()
         con.close()
+
       
-    
+def update_quantity(name, new_quantity):
+     
+    # update
+     
+     con = sqlite3.connect(dbstring)
+     cur = con.cursor()
+     cur.execute("UPDATE stock SET Quantity = ? WHERE Name = ?", (new_quantity, name))
+     con.commit()
+     con.close()
+
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
@@ -94,6 +115,35 @@ def add_item():
         }
     }), 201
 
+# Update Quantity
+@app.route('/items/patch/quantity/<string:name>', methods=['PATCH'])
+def update_item(name):
+     data = request.get_json()
+     new_quantity = data.get("Quantity")
+
+     if new_quantity is None:
+          return jsonify({"Error" : "Quantity required"}), 400
+    
+     update_quantity(name, new_quantity)
+
+     return jsonify({'Quantity Updated" : "Quantity for {name} updated to {new_quantity}'}), 200
+
+##Delete Record/Item/Row
+@app.route('/items/delete/<string:name>', methods =['DELETE'])
+def delete_item(name):
+     
+     #sets the message
+     item = retrieve_by_name(name)
+     message = {'Item Deleted':'{name}'}
+     if not item:
+          message = {"Error":"Inexistent Item"}
+
+     if name is None:
+          return jsonify({"Error": "Item name required"})
+     
+     delete_records(name)
+
+     return jsonify(message)
 
 if __name__ == "__main__":
     
@@ -104,42 +154,3 @@ if __name__ == "__main__":
 
 
 
-
-
-    # answer = ''
-    # while answer != '5':
-    #     print("Insert a number to proceed")
-    #     print("1- Insert a record \n 2- Retrieve records \n 3- Delete record  \n 4-search item  \n 5- Exit")
-        
-        
-    #     answer = input("Enter your choice: ")
-    #     if answer == '1':
-    #         name = input("Enter the name of the stock: ")
-    #         quantity = int(input("Enter the quantity: "))
-    #         price = float(input("Enter the price: "))
-    #         insert_record(name, quantity, price)
-    #         print("Record inserted successfully.")
-    #     elif answer == '2':
-    #         records = retrieve_records()
-    #         for record in records:
-    #             print(record)
-    #     elif answer == '3':
-    #         records = retrieve_records()
-    #         for record in records:
-    #             print(record)
-    #         item = input("Enter the item name to be deleted: ") 
-    #         delete_records(item)
-    #         records = retrieve_records()
-    #         for record in records:
-    #             print(record)
-            
-    #     elif answer == '4':
-    #         item = input("Search for a item: ")
-    #         records = retrieve_by_name(item)
-    #         for record in records:
-    #             print(record)
-
-    #     elif answer == '5':
-    #         print("Closing application")
-    #     else:
-    #         print("Invalid choice. Please try again.")
